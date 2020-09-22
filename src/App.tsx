f4ad6cb310eddc0123fc026025b3c3c6ff68ce7b
maqweb <balikhinv@bk.ru>
2020-09-23 01:08:45 +0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from './Todolist';
+
+function App() {
+
+    const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false },
+        { id: 4, title: "graphQl", isDone: false },
+        { id: 5, title: "Apollo", isDone: false },
+        { id: 6, title: "Rest API", isDone: false }
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title="What to learn" tasks={tasks1} />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Todolist.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+}
+
+export function Todolist(props: PropsType) {
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
+        <ul>
+            {
+                props.tasks.map(t =>
+                    <li key={t.id}>
+                        <input type="checkbox" checked={t.isDone}/> <span>{t.title}</span>
+                    </li>
+                )
+            }
+        </ul>
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
+}
